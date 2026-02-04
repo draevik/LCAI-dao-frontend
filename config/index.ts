dcdaef70fa1358791bc240fa6e1bf7253c746d44
lightchainaidev <lightchainaidev@proton.me>
2026-02-04 17:34:56 +0200

    Refactor configuration and update .gitignore
    - Updated .gitignore to explicitly include .env file.
    - Refactored config/index.ts to use environment variable for Infura API key and removed unused LCAI Testnet configuration.
    - Streamlined predefined contracts and timeLock settings to focus on mainnet only.

--- config/index.ts
+++ config/index.ts
@@ -1,43 +1,36 @@
 import { Token } from "@/types";
-import { Chain, localhost, mainnet } from "viem/chains";
+import { Chain, mainnet } from "viem/chains";
 
+const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 const customMainnet: Chain = {
   ...mainnet,
   rpcUrls: {
     default: {
-      http: ["https://mainnet.infura.io/v3/e4c15472e4824fefae8a9d5b265e8180"],
+      http: [`https://mainnet.infura.io/v3/${infuraApiKey}`],
     },
   },
 };
 
-export const lcaiTestnet: Chain = {
-  id: 504,
-  name: "LCAI Testnet",
-  nativeCurrency: {
-    name: "LCAI Testnet",
-    symbol: "LCAI",
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ["https://light-testnet-rpc.lightchain.ai"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "LCAI Testnet Explorer",
-      url: "https://testnet.lightscan.app",
-    },
-  },
-};
-
-const hardhat = {
-  ...localhost,
-  id: 31337,
-};
-
-// counter 0x610178da211fef7d417bc0e6fed39f05609ad788
-// lcai counter 0x2CA89a0add8553752e37d2f455937ca739a1069C
+// export const lcaiTestnet: Chain = {
+//   id: 504,
+//   name: "LCAI Testnet",
+//   nativeCurrency: {
+//     name: "LCAI Testnet",
+//     symbol: "LCAI",
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ["https://light-testnet-rpc.lightchain.ai"],
+//     },
+//   },
+//   blockExplorers: {
+//     default: {
+//       name: "LCAI Testnet Explorer",
+//       url: "https://testnet.lightscan.app",
+//     },
+//   },
+// };
 
 const config = {
   chains: [customMainnet] as [Chain, ...Chain[]],
@@ -52,40 +45,24 @@ const config = {
   },
 
   predefinedContracts: {
-    [customMainnet.id]: [
+    [mainnet.id]: [
       {
         name: "Lightchain Treasury",
         address: "0x07A716a551E5f4CA7D6C71Da9dF1cb1429Dba826",
       },
     ],
-    // [hardhat.id]: [
-    //   {
-    //     name: "Counter (Example)",
-    //     address: "0x2CA89a0add8553752e37d2f455937ca739a1069C",
-    //   },
-    // ],
-    // [lcaiTestnet.id]: [
-    //   {
-    //     name: "Counter (Example)",
-    //     address: "0x2CA89a0add8553752e37d2f455937ca739a1069C",
-    //   },
-    // ],
   } as Record<number, { name: string; address: `0x${string}` }[]>,
 
   timeLock: {
-    [customMainnet.id]: `0xbE1c37F8C4DA77dD06F4A8AC5098Ec70273093d7`,
-    [hardhat.id]: `0x5fbdb2315678afecb367f032d93f642f64180aa3`,
-    [lcaiTestnet.id]: `0x6FDA6BFfdf8f6ea638D1AdED3d5Bdf337dec7DAc`,
+    [mainnet.id]: `0xbE1c37F8C4DA77dD06F4A8AC5098Ec70273093d7`,
   } as Record<number, `0x${string}`>,
 
   governor: {
-    [customMainnet.id]: `0x6dfa413B5900a1a7947BC75E68AbBA093cB2492d`,
-    [hardhat.id]: `0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0`,
-    [lcaiTestnet.id]: `0x58Fc79c5DeF257b0DD86ca59b48870E343946Fe5`,
+    [mainnet.id]: `0x6dfa413B5900a1a7947BC75E68AbBA093cB2492d`,
   } as Record<number, `0x${string}`>,
 
   underlyingToken: {
-    [customMainnet.id]: {
+    [mainnet.id]: {
       address: "0x9ca8530ca349c966fe9ef903df17a75b8a778927",
       symbol: "LCAI",
       name: "LCAI",
@@ -95,17 +72,11 @@ const config = {
   } as Record<number, Token>,
 
   voteToken: {
-    [customMainnet.id]: {
+    [mainnet.id]: {
       name: "LCAIBallot",
       symbol: "LCAIB",
       decimals: 18,
       address: "0x75F3D01c4D960FE986A598B7954A3b786B29cE49",
-    },
-    [lcaiTestnet.id]: {
-      name: "LCAIBallot",
-      symbol: "LCAIB",
-      decimals: 18,
-      address: "0xF1c352E47C3c2498aA809e4d4E19295089aCF360",
     },
   } as Record<number, Token>,
 };
