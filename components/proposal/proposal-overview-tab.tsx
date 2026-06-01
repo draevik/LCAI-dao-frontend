8aa7b69beb7f30bf1e70aaf039774f11e09e3506
lightchainaidev <lightchainaidev@proton.me>
2026-06-01 14:28:31 +0200

    Update proposal-overview-tab.tsx

--- components/proposal/proposal-overview-tab.tsx
+++ components/proposal/proposal-overview-tab.tsx
@@ -27,7 +27,7 @@ export function ProposalOverviewTab({ content }: ProposalOverviewTabProps) {
   // }, [content]);
 
   return (
-    <div className="prose prose-sm max-w-none dark:prose-invert">
+    <div className="prose prose-sm max-w-none dark:prose-invert break-words">
       <Markdown>{description}</Markdown>
 
       {/* {discussion && (
